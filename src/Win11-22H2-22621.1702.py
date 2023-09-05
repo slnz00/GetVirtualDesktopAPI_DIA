28f31a0ecc5c1c37180699a3b0bf2cd993ab0ee1
slnz00 <balintbakcsa@gmail.com>
2023-09-05 22:52:24 +0200

    create dumps for various windows 11 builds

---
+++ src/Win11-22H2-22621.1702.py
@@ -0,0 +1,19 @@
+from src.components.PEFile import PEFile
+from src.components.Dumper import Dumper
+
+twinuipcshell = PEFile(r"C:\Windows\System32\twinui.pcshell.dll")
+actxprxy = PEFile(r"C:\Windows\System32\actxprxy.dll")
+
+dump = Dumper(Dumper.Output.FILE, 'Win11-22H2-22621.1702', [twinuipcshell, actxprxy])
+
+dump.description()
+
+dump.guid('IID_IVirtualDesktopManagerInternal')
+dump.guid('IID_IVirtualDesktopManager')
+dump.guid('IID_IVirtualDesktop')
+dump.guid('IID_IVirtualDesktopPinnedApps')
+
+dump.vftable('IVirtualDesktopManagerInternal', '??_7CVirtualDesktopManager@@6BIVirtualDesktopManagerInternal@@@')
+dump.vftable('IVirtualDesktopManager', '??_7VirtualDesktopsApi@@6B@')
+dump.vftable('IVirtualDesktop', '??_7CVirtualDesktop@@6BIVirtualDesktop@@@')
+dump.vftable('IVirtualDesktopPinnedApps', '??_7VirtualPinnedAppsHandler@@6B?$ChainInterfaces@UIVirtualDesktopPinnedAppsPrivate@@UIVirtualDesktopPinnedApps@@VNil@Details@WRL@Microsoft@@V3456@V3456@V3456@V3456@V3456@V3456@V3456@@WRL@Microsoft@@@')
---
+++ src/Win11Insider-23H2-25941.1000.py
@@ -0,0 +1,19 @@
+from src.components.PEFile import PEFile
+from src.components.Dumper import Dumper
+
+twinuipcshell = PEFile(r"C:\Windows\System32\twinui.pcshell.dll")
+actxprxy = PEFile(r"C:\Windows\System32\actxprxy.dll")
+
+dump = Dumper(Dumper.Output.FILE, 'Win11Insider-23H2-25941.1000', [twinuipcshell, actxprxy])
+
+dump.description()
+
+dump.guid('IID_IVirtualDesktopManagerInternal2')
+dump.guid('IID_IVirtualDesktopManager')
+dump.guid('IID_IVirtualDesktop2')
+dump.guid('IID_IVirtualDesktopPinnedApps')
+
+dump.vftable('IVirtualDesktopManagerInternal', '??_7CVirtualDesktopManager@@6BIVirtualDesktopManagerInternal2@@@')
+dump.vftable('IVirtualDesktopManager', '??_7VirtualDesktopsApi@@6B@')
+dump.vftable('IVirtualDesktop', '??_7CVirtualDesktop@@6B?$ChainInterfaces@UIVirtualDesktop2@@UIVirtualDesktop@@VNil@Details@WRL@Microsoft@@V3456@V3456@V3456@V3456@V3456@V3456@V3456@@WRL@Microsoft@@@')
+dump.vftable('IVirtualDesktopPinnedApps', '??_7VirtualPinnedAppsHandler@@6B?$ChainInterfaces@UIVirtualDesktopPinnedAppsPrivate@@UIVirtualDesktopPinnedApps@@VNil@Details@WRL@Microsoft@@V3456@V3456@V3456@V3456@V3456@V3456@V3456@@WRL@Microsoft@@@')
